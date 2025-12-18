d14ba19493ab6548618a3ee4af8ac527b08ee467
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2025-12-17 21:20:23 -0500

    lint

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,7 @@
 export { default as Button } from './components/Button.svelte';
 export { default as Counter } from './components/Counter.svelte';
-export { createCounterContext, getCounterContext, type CounterContext } from './context/counter.svelte';
+export {
+	createCounterContext,
+	getCounterContext,
+	type CounterContext
+} from './context/counter.svelte';
